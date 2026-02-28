13c2a99352f9d2e143733d7428d29e41d1c0e3b1
poornimaramesh <poornimaramesh1995@gmail.com>
2026-02-28 13:23:28 +0300

    update dealing with response from http call

--- evidential/lib/journey_events.lua
+++ evidential/lib/journey_events.lua
@@ -6,21 +6,20 @@ function Functions.get_assignment_for_contact(contact_id, experiment_id)
                               turn.app.get_config_value("evidential_api_base_url"),
                               experiment_id, 
                               contact_id)
-    local body, status_code, headers = turn.http.request({
+    local response = turn.http.request({
         url = url,
         method = "GET",
         headers = {
             ["X-API-Key"] = tostring(turn.app.get_config_value("evidential_api_key")),
         }
     })
-    print("Body: " .. body)
 
-    if status_code == 200 then
-        local response = turn.json.decode(body)
-        return response.assignment.arm_id
+    if response.status_code == 200 then
+        local response_body = turn.json.decode(response.body)
+        return response_body.assignment.arm_id
     else
-        turn.logger.error("Failed to get assignment: " .. body)
-        return nil, "Failed to get assignment: " .. body
+        turn.logger.error("Failed to get assignment: " .. response.body)
+        return nil, "Failed to get assignment: " .. response.body
     end
 end
 
@@ -29,7 +28,7 @@ function Functions.post_outcome_for_contact(contact_id, experiment_id, outcome)
                               turn.app.get_config_value("evidential_api_base_url"),
                               experiment_id, 
                               contact_id)
-    local body, status_code, headers = turn.http.request({
+    local response = turn.http.request({
         url = url,
         method = "POST",
         headers = {
@@ -39,12 +38,12 @@ function Functions.post_outcome_for_contact(contact_id, experiment_id, outcome)
         body = turn.json.encode({ outcome = outcome })
     })
 
-    if status_code == 200 then
-        local response = turn.json.decode(body)
-        return response
+    if response.status_code == 200 then
+        local response_body = turn.json.decode(response.body)
+        return response_body
     else
-        turn.logger.error("Failed to post outcome: " .. body)
-        return nil, "Failed to post outcome: " .. body
+        turn.logger.error("Failed to post outcome: " .. response.body)
+        return nil, "Failed to post outcome: " .. response.body
     end
 end
 
